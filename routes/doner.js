470b296df99022233e06ab069547393a26dead25
sarinps007 <70061082+sarinps007@users.noreply.github.com>
2020-11-16 16:57:03 +0530

    Update doner.js

--- routes/doner.js
+++ routes/doner.js
@@ -2,11 +2,21 @@ const express=require('express')
 const db=require('../database/database')
 
 
+const SELECT_REQUEST='SELECT request_id, blood_group, units, "userId", solved, date_of_request FROM public."BloodRequest" WHERE blood_group= $1 AND  solved=false '
+
 const donor=express.Router()
 donor.use(express.json())
 
-
-
+donor.route('/required').post((req,res)=>{
+    const {blood_group}=req.body
+    db.query(SELECT_REQUEST,[blood_group])
+    .then(data=>{
+        return res.status(200).json(data.rows)
+    })
+    .catch(err=>{
+        return res.status(400).json({message:"Something went wrong"})
+    })
+})
 
 
 
