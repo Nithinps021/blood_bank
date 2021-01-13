cee7ddb29d4b16849f3beaef8c392d74707f2aa4
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2021-01-13 23:29:04 +0530

    added routes for donor

--- routes/donor.js
+++ routes/donor.js
@@ -1,22 +1,65 @@
-const express=require('express')
-const db=require('../database/database')
+const express = require("express");
+const { compile } = require("morgan");
+const { all, use } = require("../app");
+const { query } = require("../database/database");
+const db = require("../database/database");
 
+const SELECT_REQUEST =
+  'SELECT request_id, blood_group, units, "userId", solved, date_of_request FROM public."BloodRequest" where blood_group=$1 and request_id not in ( SELECT request_id FROM public."DonorsWilling");';
+const INSERT_WILLING =
+  'INSERT INTO public."DonorsWilling"( username, request_id) VALUES ($1,$2);';
+const DELETE_WILLING =
+  'DELETE FROM public."DonorsWilling" WHERE username=$1 and request_id=$2;';
 
-const SELECT_REQUEST='SELECT request_id, blood_group, units, "userId", solved, date_of_request FROM public."BloodRequest" WHERE blood_group= $1 AND  solved=false '
+const PAST_DONATIONS =
+  'SELECT request_id, blood_group, units, "userId", solved, date_of_request FROM public."BloodRequest" where solved=true and request_id in (select request_id FROM public."DonorsWilling" where username=$1);';
+const donor = express.Router();
+donor.use(express.json());
 
-const donor=express.Router()
-donor.use(express.json())
-
-donor.route('/not_fullfilled').post((req,res)=>{
-    const {blood_group}=req.body
-    db.query(SELECT_REQUEST,[blood_group])
-    .then(data=>{
-        return res.status(200).json(data.rows)
+donor.route("/not_fullfilled").post((req, res) => {
+  const { blood_group, username } = req.body;
+  db.query(SELECT_REQUEST, [blood_group])
+    .then((data1) => {
+      let finalData = data1.rows.filter(
+        (e) => e.userId.trim() != username.trim()
+      );
+      return res.status(200).json(finalData);
     })
-    .catch(err=>{
-        return res.status(400).json({message:"Something went wrong"})
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ message: "Something went wrong", err });
+    });
+});
+donor.route("/willing").post((req, res) => {
+  const { username, request_id } = req.body;
+  db.query(INSERT_WILLING, [username, request_id])
+    .then((data) => {
+      return res.status(200).json({ added: true, data });
     })
-})
+    .catch((err) => {
+      return res.status(400).json({ message: "Not added", added: false, err });
+    });
+});
+donor.route("/not_willing").post((req, res) => {
+  const { username, request_id } = req.body;
+  db.query(DELETE_WILLING, [username, request_id])
+    .then((data) => {
+      return res.status(200).json({ removed: true, data });
+    })
+    .catch((err) => {
+      return res.status(400).json({ removed: false, err });
+    });
+});
 
-module.exports=donor
+donor.route("/past_donations").post((req, res) => {
+  const { username } = req.body;
+  db.query(PAST_DONATIONS, [username])
+    .then((data) => {
+      return res.status(200).json(data.rows);
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: "Someting went wrong", err });
+    });
+});
 
+module.exports = donor;
--- routes/request.js
+++ routes/request.js
@@ -10,6 +10,14 @@ const SELECT_BLOOD_REQUEST =
   'SELECT request_id, blood_group, units, "userId", solved, date_of_request FROM public."BloodRequest" WHERE "userId"=$1 and solved=$2;';
 const UPDATE_REQUEST =
   'UPDATE public."BloodRequest" SET blood_group=$1, units=$2, solved=$3 WHERE request_id=$4;';
+const CANCEL_REQUEST =
+  'UPDATE public."BloodRequest" SET solved=$1 WHERE request_id=$2;';
+const COLLECT_USER_INFO =
+  'SELECT dob, email, gender, "phoneNo",selected,public."User".username FROM public."User" inner join public."DonorsWilling" on public."User".username=public."DonorsWilling".username where request_id=$1;';
+const SELECT_DONOR =
+  'UPDATE public."DonorsWilling" SET  selected=true WHERE username=$1 and request_id=$2;';
+const DESELECT_DONOR =
+  'UPDATE public."DonorsWilling" SET  selected=false WHERE username=$1 and request_id=$2;';
 
 request.route("/").post((req, res) => {
   const { username: userId, units, blood_group, date_of_request } = req.body;
@@ -55,14 +63,56 @@ request.route("/resolved/:username").get((req, res) => {
 });
 
 request.route("/update").post((req, res) => {
-    const {solved,request_id,blood_group,units}=req.body
-    db.query(UPDATE_REQUEST,[blood_group,units,solved,request_id])
-    .then(()=>{
-        return res.status(200).json({message:"Updated",updated:true})
+  const { solved, request_id, blood_group, units } = req.body;
+  db.query(UPDATE_REQUEST, [blood_group, units, solved, request_id])
+    .then(() => {
+      return res.status(200).json({ message: "Updated", updated: true });
     })
-    .catch(err=>{
-        return res.status(400).json({message:"Not updated",updated:false})
+    .catch((err) => {
+      return res.status(400).json({ message: "Not updated", updated: false });
+    });
+});
+request.route("/delete_request").post((req, res) => {
+  const { request_id } = req.body;
+  db.query(CANCEL_REQUEST, [true, request_id])
+    .then((data) => {
+      return res.status(200).json({ removed: true, data });
     })
+    .catch((err) => {
+      return res
+        .status(400)
+        .json({ message: "something went wrong", removed: false });
+    });
+});
+request.route("/donors_details").post((req, res) => {
+  const { request_id } = req.body;
+  db.query(COLLECT_USER_INFO, [request_id])
+    .then((data) => {
+      return res.status(200).json(data.rows);
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: "Someting went wrong", err });
+    });
+});
+request.route("/select").post((req, res) => {
+  const { username, request_id } = req.body;
+  db.query(SELECT_DONOR, [username, request_id])
+    .then((data) => {
+      return res.status(200).json({ seleted: true, data });
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: "something went wrong", err });
+    });
+});
+request.route("/deselect").post((req, res) => {
+  const { username, request_id } = req.body;
+  db.query(DESELECT_DONOR, [username, request_id])
+    .then((data) => {
+      return res.status(200).json({ deseleted: true, data });
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: "something went wrong", err });
+    });
 });
 
 module.exports = request;
